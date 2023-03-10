d004853978411a86e3598dd2604b0fafd0520ab0
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-03-10 12:35:39 +0100

    page contact + emailjs branché tout fonctionne avec un style boostrap simple mais clean

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,8 +5,8 @@ import FormEmail from '../components/FormEmail';
 const Contact = () => {
     return (
         <div>
-            <FormEmail/>
             <h1>Contact</h1>
+            <FormEmail/>
         </div>
     );
 };
