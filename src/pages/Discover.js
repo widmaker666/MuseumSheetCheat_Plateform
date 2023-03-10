96af68cb4353e98039f3d875f0cce3a6e5cc3ed5
KyleAktr <aktr.contact@gmail.com>
2023-03-10 18:04:18 +0100

    front welcome responsive ok, front discover not responsive

--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React from "react";
+import Navigation from "../components/Navigation";
+import passion from "../assets/images/la-passion.jpg";
+import dali from "../assets/images/Dali.jpg";
+
 
 const Discover = () => {
-    return (
-        <div>
-            <h1>Présentation du site + API</h1>
-        </div>
-    );
+  return (
+    <div className="discover">
+      <Navigation />
+      <h1 className="what">What are you doing here?</h1>
+      <img className="la-passion" src={passion} alt="image peinture Van Gogh" />
+      <p className="descri">
+        Art is an activity, the product of that activity or the idea of it,
+        which deliberately addresses the senses, emotions, intuitions and the
+        intellect.
+        <br />
+        <br />
+        Museum Sheet Cheat was created to collect a large number of artistic
+        works from all periods. MSC is aimed at all art lovers, students, the
+        curious, and anyone wishing to learn more about an artistic movement, an
+        artist, an artistic work...
+        <br />
+        <br />
+        Art is to be seen, but also discussed. That is why you will find a
+        chatroom where everyone is free to express their thoughts.{" "}
+      </p>
+      <img className="dali" src={dali} alt="image peinture Van Gogh" />
+      <h1 className="why">Why Subscribe ?</h1>
+        <p className="sub">
+          - You will be able to store your favorite works so that you can find
+          them more easily <br /> <br />
+          - Have a nickname is convenient to use a chat <br />
+          <br />- This site is a study project and we need some statistical
+          data.
+        </p>
+      
+    </div>
+  );
 };
 
 export default Discover;
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,9 +1,44 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Navigation from "../components/Navigation";
+import matisse from "../assets/images/matisse.jpg";
+import fenetre from "../assets/images/fenetre.jpg";
+import ambush from "../assets/images/ambush1.jpg";
 
 const Welcome = () => {
   return (
-    <div>
-      <h1>Welcome</h1>
+    <div className="welcome">
+      <Navigation />
+      <h1 className="h1_welcome">
+        Do you know <br /> Museum sheet cheat ?
+      </h1>
+      <div className="buttons-welcome">
+        <div className="to_discover">
+          <h3 className="click-discover">Click here to discover</h3>
+          <NavLink to="/discover">
+            <img
+              className="img_matisse"
+              src={matisse}
+              alt="image peinture matisse"
+            />
+          </NavLink>
+        </div>
+        <div className="to_start">
+          <h3 className="click-start">Click here to start</h3>
+          <NavLink to="/login">
+            <img
+              className="img_fenetre"
+              src={fenetre}
+              alt="image peinture matisse"
+            />
+          </NavLink>
+        </div>
+      </div>
+      <img
+        className="img_harley"
+        src={ambush}
+        alt="image harley weir"
+      />
     </div>
   );
 };
