f8abf26670045afce2147d07808a03036abc5da3
KyleAktr <aktr.contact@gmail.com>
2023-03-14 16:15:30 +0100

    front sign up not responsive

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { auth, db } from "../config/firebase-congif";
-
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { ref, set } from "firebase/database";
-
-
+import signup_img from "../assets/images/signup_img.jpg";
+import Navigation from "../components/Navigation";
 
 const SignUp = () => {
   const [firstName, setFirstName] = useState("");
@@ -33,32 +32,42 @@ const SignUp = () => {
 
   return (
     <div>
-  
-      <form className="signupForm" onSubmit={handleSubmit}>
-        <input
-          placeholder="first name"
-          onChange={(e) => setFirstName(e.target.value)}
-          required
-        ></input>
-        <input
-          placeholder="last name"
-          onChange={(e) => setLastName(e.target.value)}
-          required
-        ></input>
-        <input
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-          required
-          type="email"
-        ></input>
-        <input
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          type="password"
-        ></input>
-        <button>Sign Up</button>
-      </form>
+      <div className="login">
+        <Navigation/>
+        <div className="form_container">
+          <div className="formulaire">
+            <form className="loginForm" onSubmit={handleSubmit}>
+              <h2>Sign up</h2>
+              <p>first name</p>
+              <input
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+              ></input>
+              <p>last name</p>
+              <input
+                onChange={(e) => setLastName(e.target.value)}
+                required
+              ></input>
+              <p>email</p>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                type="email"
+              ></input>
+              <p>password</p>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                type="password"
+              ></input>
+              <div className="btn">
+                <button>Sign Up</button>
+              </div>
+            </form>
+          </div>
+          <img className="img_form" src={signup_img} alt="image" />
+        </div>
+      </div>
     </div>
   );
 };
