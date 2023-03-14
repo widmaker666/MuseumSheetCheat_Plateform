a2b61662831e01df471a2e44f7bcb30d626ea2d8
KyleAktr <aktr.contact@gmail.com>
2023-03-14 11:49:23 +0100

    front login page not responsive

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-
 import { useNavigate } from "react-router-dom";
 import { auth } from "../config/firebase-congif";
-
-
+import login_img from "../assets/images/login_img.jpg";
+import Navigation from "../components/Navigation";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -24,20 +23,30 @@ const Login = () => {
 
   return (
     <div>
-    
-      <form className="loginForm" onSubmit={handleSubmit}>
+      <Navigation />
+      <div className="form_container">
+        <div className="formulaire">
+        <form className="loginForm" onSubmit={handleSubmit}>
+          <h2>Login</h2>
+          <p>Email</p>
         <input
-          placeholder="email"
+          
           type="email"
           onChange={(e) => setEmail(e.target.value)}
         ></input>
+        <p>Password</p>
         <input
-          placeholder="password"
+          
           type="password"
           onChange={(e) => setPassword(e.target.value)}
         ></input>
+        <div className="btn">
         <button>Login</button>
+        </div>
       </form>
+        </div>
+        <img className="img_form" src={login_img} alt="image" />
+      </div>
     </div>
   );
 };
