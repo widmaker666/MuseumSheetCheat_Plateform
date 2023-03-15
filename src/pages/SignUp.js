9e9941337962c7fdf80530c868c57580d5a65be4
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-03-15 10:29:52 +0100

    validation fonctionnelle pour le signup

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -11,10 +11,31 @@ const SignUp = () => {
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const [validation, setValidation] = useState("");
+  const [validationPassword, setValidationPassword] = useState("");
+
+  console.log(password);
+  const passwordHasLowercaseLetter = /[a-z]/.test(password);
+  const passwordHasUppercaseLetter = /[A-Z]/.test(password);
+  const passwordHasSpecialCharacter = /^(?=.*[!@#\$%\^&\*])/.test(password);
+  const passwordHasNumber = /[0-9]/.test(password);
+  const passwordHasValidLength = password.length >= 8;
+
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault();  
+    
+    if (password.length < 8 || !/[a-z]/.test(password) || !/[A-Z]/.test(password) || !/[0-9]/.test(password) || !/[^a-zA-Z0-9]/.test(password)) {
+      setValidationPassword("Password need to be more secured");      
+      return;
+    }else if (password !== repeatPassword) {
+      setValidation("Passwords must be the same");      
+      return;
+    }
+    
+
     function onRegister() {
       createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -25,26 +46,29 @@ const SignUp = () => {
           });
         })
         .catch((error) => console.log(error));
-      navigate("/home");
+        navigate('/home')    
     }
     onRegister();
+    
   };
 
   return (
     <div>
       <div className="login">
-        <Navigation/>
+        <Navigation />
         <div className="form_container">
           <div className="formulaire">
             <form className="loginForm" onSubmit={handleSubmit}>
               <h2>Sign up</h2>
               <p>first name</p>
               <input
+                type="text"
                 onChange={(e) => setFirstName(e.target.value)}
                 required
               ></input>
               <p>last name</p>
               <input
+                type="text"
                 onChange={(e) => setLastName(e.target.value)}
                 required
               ></input>
@@ -60,6 +84,51 @@ const SignUp = () => {
                 required
                 type="password"
               ></input>
+              <p className="text-danger mt-1">{validationPassword}</p>
+              <ul>
+                <li
+                  style={{
+                    color: passwordHasLowercaseLetter ? "green" : "red",
+                  }}
+                >
+                  One lowerCase letter
+                </li>
+                <li
+                  style={{
+                    color: passwordHasUppercaseLetter ? "green" : "red",
+                  }}
+                >
+                  One UpperCase letter
+                </li>
+                <li
+                  style={{
+                    color: passwordHasSpecialCharacter ? "green" : "red",
+                  }}
+                >
+                  One special character
+                </li>
+                <li style={{ color: passwordHasNumber ? "green" : "red" }}>
+                  One number
+                </li>
+                <li
+                  style={{
+                    color: passwordHasValidLength ? "green" : "red",
+                  }}
+                >
+                  minimum 8 characters
+                </li>
+              </ul>
+              <div className="mb-3">
+                <label htmlFor="repeatPwd" className="form-label">
+                  Repeat Password
+                </label>
+                <input
+                  required
+                  type="password"
+                  onChange={(e) => setRepeatPassword(e.target.value)}
+                />
+              </div>
+              <p className="text-danger mt-1">{validation}</p>
               <div className="btn">
                 <button>Sign Up</button>
               </div>
