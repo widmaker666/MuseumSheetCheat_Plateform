32c6ba33dcdccb275bc366946080e99801d8af7f
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-03-18 12:07:56 +0100

    ajout npm i ! puis ajout text anime sur page home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,13 +7,18 @@ import { ref, onValue } from "firebase/database";
 import PaintCard from "../components/PaintCard";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import TypeWriterEffect from "react-typewriter-effect";
 
 
 function Home() {
+  //!Constants
   const { currentUser } = useContext(AuthContext);
   const [username, setUsername] = useState("");
   const navigate = useNavigate();
 
+  //!Functions textAnime
+
+  //!Fonctions
   useEffect(() => {
     if (currentUser) {
       const starCountRef = ref(db, "users/" + currentUser.uid);
@@ -38,20 +43,43 @@ function Home() {
   const logOut = () => navigate("/login");
 
   return (
-    <div className="home">    
+    <div className="home">
       <Navbar />
       <br />
       <br />
       <br />
       <br />
+      <div >
+        <h3>Bienvenue sur Museum Sheet Cheat</h3>
+        <h5 className="justify-content-center">
+        <TypeWriterEffect className="text-align-center"
+        textStyle={{
+          fontFamily: "Roboto",
+          color: 'white',
+          fontWeight: 500,
+          fontSize: '1.5em',
+        }}
+        startDelay={2000}
+        cursorColor="#3F3D56"
+        multiText={[
+          'The only real in the art, it is the art',
+          'Art is the image of creation. It is a symbol, just as the earthly world is a symbol of the cosmos',
+          'Art is the shortest way from man to man',
+          'True art is not only the expression of a feeling, but also the result of a sharp intelligence',
+          'The work of art is not the reflection, the image of the world; but it is the image of the world',          
+          'Enjoy your visit to the Museum Sheet Cheat website'
+        ]}
+        multiTextDelay={2000}
+        typeSpeed={100}
+      />
+        </h5>
+      </div>
       <br />
       <br />
       <br />
       <br />
       <div className="mainContainer mb-5">
-        {currentUser && (
-          <p>{`Welcome to the Museum Sheet Cheat, you're connected now ! Nice visit ${username}`}</p>
-        )}
+        {currentUser && <p>{"Connected"}</p>}
         <div className="buttons">
           <button onClick={clickLogin}>
             {currentUser ? (
